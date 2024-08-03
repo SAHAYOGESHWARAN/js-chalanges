e240d0870f3369c7833ad3a6569e4b1b872cc78d
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-03 17:05:26 +0530

    add jschallengers master

---
+++ casll back.js
@@ -0,0 +1,17 @@
+function welcomeUser(username) {
+    console.log(`Welcome ${username}`);
+  }
+  function getName() {
+    let name;
+    setTimeout(function() {
+      name = 'Jane';
+    }, 500);
+    return name;
+  }
+  const result = getName();
+  welcomeUser(result);
+
+
+
+
+  //output welcome undefined
