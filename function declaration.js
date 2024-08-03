a4f2d66d238fe3914bd67e73dadbeb3c32f72c9f
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-03 13:09:53 +0530

    add jschallengers master

---
+++ function declaration.js
@@ -0,0 +1,19 @@
+//In this exercise we create a function called func. Then we call the function and assign its return value to the variable result. To solve this exercise, simply have the console.log() statement log the words hello world.
+
+
+//question
+
+//function func() {
+  //  return 'hello world';
+ //};
+ //const result = func();
+ //console.log(result);
+ 
+
+ //answer
+ function func() {
+    return 'hello world';
+    }
+    const result = func();
+    console.log(result);
+    //  output: hello world.
