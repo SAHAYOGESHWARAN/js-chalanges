fa9c08793b7ea2dde97519a5f17a272482a03707
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-03 11:46:17 +0530

    add jschallengers master

---
+++ boolean to variable.js
@@ -0,0 +1,16 @@
+//Here, we declare the variable isTrue. But, it has no value yet. 
+//Assign a boolean value to it and run the code.
+
+
+//let isTrue;
+
+//console.log(isTrue);
+
+
+
+let isTrue = true;
+
+console.log(isTrue);
+
+//output
+//true
---
+++ reassign a value a variable(2).js
@@ -0,0 +1,19 @@
+//The code below first declares a variable named num with value 1.
+/// Then, a new value 2 is assigned. Finally, the variable is logged.
+//But, the code will not work like that. 
+///Find the mistake and fix it. Execute the corrected code.
+
+
+
+//const num = 1;
+//num = 2;
+//console.log(num);
+
+
+
+const 
+num = 2;
+console.log(num);
+
+//output 
+//2
