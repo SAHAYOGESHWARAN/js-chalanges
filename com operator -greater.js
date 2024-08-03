ef7367ba2f4af8f82dbb6be3929cb6f71bc63643
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-03 12:30:12 +0530

    add jschallengers master

---
+++ com operator -greater.js
@@ -0,0 +1,15 @@
+//In the console.log() statement below we use the Greater Than operator to check whether the value of numOne is greater than the value of numTwo.
+ //Change the code so that the console.log() statement logs true.
+
+ 
+//const numOne = 5;
+//const numTwo = 6;
+//console.log(numOne > numTwo);
+
+
+
+const numOne = 15;
+const numTwo = 6;
+console.log(numOne > numTwo);
+
+//output : true
