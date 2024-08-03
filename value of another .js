b9438e18b3da9cc11bc004a654b3f38af8866732
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-03 11:22:58 +0530

    add js challenge master

---
+++ value of another .js
@@ -0,0 +1,13 @@
+//Here, we have two variables numOne and numTwo. numOne already has a value.
+ //Assign numTwo the value of numOne and run the code.
+
+ //let numOne = 5;
+
+//console.log(numTwo);
+
+let numOne = 5;
+let numTwo = numOne;
+console.log(numTwo);
+
+//output
+//5
