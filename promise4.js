7c0cd7dc37685299630a6b8200f740614c6f2002
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-03 17:27:13 +0530

    add jschallengers master

---
+++ promise4.js
@@ -0,0 +1,15 @@
+new Promise(function(resolve, reject) {
+    console.log('Starting task...');
+    setTimeout(function() {
+      resolve();
+    }, 1000);
+  }).then(function () {
+    console.log('Promise fulfilled');
+  });
+  console.log('I am not blocked');
+ 
+  // output 
+
+  //Starting task...
+//I am not blocked
+//Promise fulfilled
