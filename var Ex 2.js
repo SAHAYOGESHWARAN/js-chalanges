5afebdd4d4a98bdf1c18a5ad2820bf28a5bc77a0
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-03 11:18:51 +0530

    add jschallenge master

---
+++ var Ex 2.js
@@ -0,0 +1,14 @@
+//Here, we declare the variable num. 
+//But, it has no value yet. Assign a value to it and run the code.
+
+
+// let num;
+
+// console.log(num);
+
+let num;
+num = 2;
+console.log(num);
+
+//output 
+// 2
