7f9b4fd5e5b7254c4e755fb558d600d19f33c5df
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-03 12:48:28 +0530

    add jschallengers master

---
+++ creating string.js
@@ -0,0 +1,14 @@
+//In this exercise the existing console.log() statement logs the value of the variable text. The variable text has already been declared with an empty string – as indicated by the two single quotes.
+//Fill in the string with some characters and run the code to see if the string is being logged.
+
+
+// question
+//const text = '';
+//console.log('The value of text is: ' + text);
+
+
+// answer
+const text = 'hello world';
+console.log('The value of text is: ' + text);
+
+//output The value of text is: hello world
