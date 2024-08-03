37acdd4d20a3c674f4335d5af07020173c074017
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-03 13:03:51 +0530

    ad jschallengers master

---
+++ if statement .js
@@ -0,0 +1,20 @@
+//The code will assign the variable num a new value 1. But it will only run if the condition is met.
+//Adjust the condition such that the code inside the curly braces will execute and the 
+//console.log() statement logs true.
+
+//question
+
+l//et num = 0;
+//if (1 > 2) {
+  // num = 1;
+///}
+//console.log(num === 1);
+
+//answer
+let num = 0;
+if (2 > 1) {
+    num = 1;
+    }
+    console.log(num === 1); 
+    
+    // This will log true.
---
+++ if statement add missing.js
@@ -0,0 +1,20 @@
+//Time to practice what we've learnt so far. In the code below, the if...statement will assign a new value to the variable text. But only if its condition is met. Currently, the condition is missing.
+//Add any condition that will be satisfied such that the console.log() statement logs true.
+
+//question
+
+
+//let text = 'hello';
+//if () {
+  // text = text + ' world';
+//}
+//console.log(text === 'hello world');
+
+
+//answer
+let text = 'hello';
+if (text === 'hello') {
+   text = text + ' world';
+}
+console.log(text === 'hello world');
+// output : true
