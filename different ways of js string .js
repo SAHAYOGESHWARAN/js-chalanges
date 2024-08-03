e9610c01de3ee98be4d5e709f6c6e5b830c84a0e
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-03 12:53:38 +0530

    add jschallengers master

---
+++ different ways of js string .js
@@ -0,0 +1,20 @@
+//Here, we have declared 3 variables textOne, textTwo, and textThree. An empty string is assigned to all of them.
+//Do you see how in each case different symbols are used to create the string? All three of them are valid methods to create a JavaScript string.
+//Fill in all 3 strings with some characters and run the code to see if the values get logged.
+
+//question
+
+//const textOne = '';
+//const textTwo = "";
+//const textThree = ``;
+//console.log(textOne, textTwo, textThree);
+
+//answer
+const textOne = 'Hello, ';
+const textTwo = "it's ";
+const textThree = `me`;
+console.log(textOne, textTwo, textThree);
+
+
+
+// output : Hello,  it's  me
