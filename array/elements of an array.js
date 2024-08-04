566d8c8f636494be8fb11e22838ddc55644497dd
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-04 12:30:25 +0530

    add jschallengers msater

---
+++ array/elements of an array.js
@@ -0,0 +1,10 @@
+function myFunction(a) {
+    return a.slice(0, 3);
+}
+
+console.log(myFunction([1, 2, 3, 4, 5, 6])); 
+
+
+// [1, 2, 3]
+
+  
---
+++ array/return last n array ele.js
@@ -0,0 +1,11 @@
+function myFunction(a, n) {
+    return a.slice(-n);
+}
+
+console.log(myFunction([1, 2, 3, 4, 5], 3));
+
+
+
+//output
+
+// [3, 4, 5]
