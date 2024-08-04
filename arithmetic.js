8ac078dab6ec88e1e0700eb44ba5c7856728765a
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-04 11:56:26 +0530

    add jschallengers master

---
+++ arithmetic.js
@@ -0,0 +1,19 @@
+
+function myFunction(a, b) {
+    if (a < b) {
+        return a / b;
+    } else {
+        return a * b;
+    }
+}
+console.log(myFunction(4, 8)); 
+console.log(myFunction(10, 5)); 
+console.log(myFunction(3, 3)); 
+console.log(myFunction(7, 14));
+
+
+//output
+//.5
+//50
+//9
+//0.5
