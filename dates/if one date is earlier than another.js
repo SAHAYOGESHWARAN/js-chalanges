47c05184e7f60399257223be8a9bcedfdd772e0e
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-05 10:12:04 +0530

    add jschallengers master

--- dates/if one date is earlier than another.js
+++ dates/if one date is earlier than another.js
@@ -2,3 +2,11 @@ function myFunction(a, b) {
     return a.getTime() < b.getTime();
   }
    
+
+  const date1 = new Date('2024-08-05T10:00:00');
+const date2 = new Date('2024-08-05T12:00:00');
+const date3 = new Date('2024-08-05T08:00:00');
+
+console.log(myFunction(date1, date2)); // Output: true
+console.log(myFunction(date2, date1)); // Output: false
+console.log(myFunction(date3, date1)); // Output: true
