e74a06d104f9729f99342a1856b5667a1d22060f
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-05 09:48:25 +0530

    add jschallengers master

--- async/Merge two objects with matching keys.js
+++ async/Merge two objects with matching keys.js
@@ -10,3 +10,12 @@ function myFunction(x, y) {
     };
   }
   
+
+  const obj1 = { a: 1, b: 2, c: 3 };
+const obj2 = { b: 20, d: 4, e: 5 };
+
+console.log(myFunction(obj1, obj2)); 
+
+
+
+// Output: { a: 1, b: 2, c: 3, d: 20, e: 5 }
