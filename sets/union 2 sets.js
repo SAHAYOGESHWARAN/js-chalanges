3c3b3cffa9b6db0438cfd357ce9629676294b0a6
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-05 10:38:54 +0530

    add jschallengers master

--- sets/union 2 sets.js
+++ sets/union 2 sets.js
@@ -1,6 +1,13 @@
-
 function myFunction(a, b) {
-  const result = new Set(a);
-  b.forEach((el) => result.add(el));
-  return result;
-}
+   
+    return new Set([...a, ...b]);
+  }
+  const setA = new Set([1, 2, 3]);
+  const setB = new Set([3, 4, 5]);
+  
+  console.log(myFunction(setA, setB)); 
+
+
+  
+  // Output: Set { 1, 2, 3, 4, 5 }
+  
