3b16a8c0d9522608ce524cdceff263a80739f5ec
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-05 11:21:11 +0530

    add jschallengers master

--- sets/js sets.js
+++ sets/js sets.js
@@ -1,8 +1,10 @@
-
 function myFunction(a, b, c) {
-    const set = new Set();
-    set.add(a);
-    set.add(b);
-    set.add(c);
-    return set;
+    return new Set([a, b, c]);
   }
+    
+
+  console.log(myFunction(1, 2, 3));         // Output: Set { 1, 2, 3 }
+console.log(myFunction(1, 1, 2));         // Output: Set { 1, 2 }
+console.log(myFunction('a', 'b', 'a'));   // Output: Set { 'a', 'b' }
+console.log(myFunction(1, 'text', true)); // Output: Set { 1, 'text', true }
+ -u 
