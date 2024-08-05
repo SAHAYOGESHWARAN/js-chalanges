5a69a2f129b166b42ae34271f8bc86a250f8fc95
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-05 09:30:31 +0530

    add jschallengers master

---
+++ async/Extract keys from Javascript object.js
@@ -0,0 +1,8 @@
+function myFunction(a) {
+    return Object.keys(a);
+  }
+  const obj = { name: 'saha', age: 20, city: 'india' };
+console.log(myFunction(obj));
+
+
+// Output: ['name', 'age', 'city']
