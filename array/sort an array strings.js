0d216732956735781a4181d4b98cda10cd24aa60
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-05 08:38:40 +0530

    add jschallengers master

---
+++ array/sort an array strings.js
@@ -0,0 +1,11 @@
+function myFunction(arr) {
+    return arr.sort();
+}
+
+
+let exampleArray = ["banana", "apple", "cherry", "date"];
+console.log(myFunction(exampleArray)); 
+
+
+
+// Output: ["apple", "banana", "cherry", "date"]
