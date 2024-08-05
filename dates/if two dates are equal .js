b2833c7c3416707e07b70fa6acd6e243d581c20c
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-05 09:55:59 +0530

    add jschallengers master

--- dates/if two dates are equal .js
+++ dates/if two dates are equal .js
@@ -1,4 +1,9 @@
 function myFunction(a, b) {
     return a.getTime() === b.getTime();
   }
-  
+  const date1 = new Date('2024-08-05T10:00:00');
+const date2 = new Date('2024-08-05T10:00:00');
+const date3 = new Date('2024-08-05T11:00:00');
+
+console.log(myFunction(date1, date2)); // Output: true
+console.log(myFunction(date1, date3)); // Output: false
