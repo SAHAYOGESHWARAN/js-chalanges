f717dff41b73588bb21e5d44e081b649d3cdc716
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-05 12:07:15 +0530

    add jschallenge master

--- sets/delete ele from set.js
+++ sets/delete ele from set.js
@@ -3,4 +3,7 @@ function myFunction(set, val) {
     return set;
   }
   
-  
+  const mySet = new Set([1, 2, 3, 4]);
+
+console.log(myFunction(mySet, 3)); // Output: Set { 1, 2, 4 }
+console.log(myFunction(mySet, 5)); // Output: Set { 1, 2, 4 }
