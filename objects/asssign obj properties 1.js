a713981fefc99a082b9a67d1343a6f2498d451f3
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-05 09:01:28 +0530

    add jschalengers master

---
+++ objects/asssign obj properties 1.js
@@ -0,0 +1,11 @@
+function myFunction(obj) {
+    return obj.country;
+}
+
+let exampleObject = { country: "india", capital: "delhi" };
+console.log(myFunction(exampleObject)); 
+
+
+
+
+// Output: "india"
