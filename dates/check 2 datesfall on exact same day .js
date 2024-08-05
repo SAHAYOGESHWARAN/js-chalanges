f05c055422cc54ea76d0d498e97347fa9a0bef4d
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-05 10:02:50 +0530

    add jschallengers master

--- dates/check 2 datesfall on exact same day .js
+++ dates/check 2 datesfall on exact same day .js
@@ -3,4 +3,11 @@ function myFunction(a, b) {
            a.getMonth() === b.getMonth() &&
            a.getDate() === b.getDate();
   }
-  
+  const date1 = new Date('2024-08-05');
+const date2 = new Date('2024-08-05');
+const date3 = new Date('2024-08-06');
+const date4 = new Date('2023-08-05');
+
+console.log(myFunction(date1, date2)); // Output: true
+console.log(myFunction(date1, date3)); // Output: false
+console.log(myFunction(date1, date4)); // Output: false
