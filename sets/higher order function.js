9f63848ae0915f93f99ed4e08347d71b90354337
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-06 08:42:49 +0530

    js

---
+++ sets/higher order function.js
@@ -0,0 +1,4 @@
+const firstOrderFunc = () =>
+    console.log("Hello, I am a First order function");
+  const higherOrder = (ReturnFirstOrderFunc) => ReturnFirstOrderFunc();
+  higherOrder(firstOrderFunc);
